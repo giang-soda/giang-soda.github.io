6ec93717fa623b56a6321d054d73482ca00afefe
Giang Soda <giang-nghiem@dimage.co.jp>
2025-07-01 16:36:28 +0700

    test subpath

--- assets/url-path-Cs_TWY_7.js
+++
@@ -1,1 +0,0 @@
-const e={DASHBOARD:"/dashboard",USERS:{LIST:"/users",CREATE:"/users/create",EDIT:s=>`/users/${s}`},EDITOR:{CKEDITOR:"/editor/ckeditor"}};export{e as U};
---
+++ xxx/assets/url-path-CdspJQV1.js
@@ -0,0 +1,1 @@
+const s="/xxx/",r={DASHBOARD:`${s}dashboard`,USERS:{LIST:`${s}users`,CREATE:`${s}users/create`,EDIT:e=>`${s}users/${e}`},EDITOR:{CKEDITOR:`${s}editor/ckeditor`}};export{s as B,r as U};
